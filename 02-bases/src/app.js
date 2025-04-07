4b3da855643684495a9a4f23271dbe8874c27ef2
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-04-07 14:04:45 -0400

    Callbacks

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,6 +1,11 @@
+const {getUserById} = require('./js-foundation/03-callbacks.js')
 
-const {emailTemplate} = require('./js-foundation/01-template.js')
-require('./js-foundation/02-destructuring.js')
+const id = 3;
 
+getUserById(id, (err, user) => {
+    if (err) {
+        throw new Error(err);
+    } 
 
-// console.log(emailTemplate);
+    console.log(user);
+})
---
+++ 02-bases/src/js-foundation/03-callbacks.js
@@ -0,0 +1,27 @@
+const users = [
+    {
+        id: 1,
+        name: 'John',
+        age: 25,
+        hobbies: ['reading', 'coding', 'hiking'],
+        isActive: true,
+    },{
+        id: 2,
+        name: 'Jane',
+        age: 30,
+        hobbies: ['painting', 'swimming', 'cooking'],
+    }
+]
+
+function getUserById(id, callback) {    
+    const user = users.find(user => user.id === id)
+
+    if (!user) {
+        return callback(new Error(`User not found with id ${id}`))
+    } 
+    callback(null, user)
+}
+
+module.exports = {
+    getUserById
+}
