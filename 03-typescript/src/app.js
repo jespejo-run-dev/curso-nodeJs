dfa38dbbb2d716d2870f940626b1c2cb00ea15d1
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-04-09 10:45:53 -0400

    TypeScript - proyecto basico

---
+++ 03-typescript/src/app.js
@@ -0,0 +1,29 @@
+const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        universe: 'DC',
+    },
+    {
+        id: 2,
+        name: 'Superman',
+        universe: 'DC',
+    },
+    {
+        id: 3,
+        name: 'Spiderman',
+        universe: 'Marvel',
+    },
+    {
+        id: 4,
+        name: 'Ironman',
+        universe: 'Marvel',
+    },
+];
+
+const findHeroById = (id) => {
+    return heroes.find((hero) => hero.id === id);
+};
+
+const hero = findHeroById(5);
+console.log(hero?.name ?? 'Hero not found');
