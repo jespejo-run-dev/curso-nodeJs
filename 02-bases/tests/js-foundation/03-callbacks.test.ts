3cb2f56e1b0e81defc8822f1162bdcc92f29865e
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-04-14 14:55:34 -0400

    Pruebas en 03-Callbacks - Exito

--- 02-bases/tests/js-foundation/03-callbacks.test.ts
+++ 02-bases/tests/js-foundation/03-callbacks.test.ts
@@ -16,4 +16,33 @@ describe('js-foundation/03-callbacks', () => {
     
     })
 
+    test('getUserById should return the user if it exists', (done) => {
+
+        
+        const id = 1;
+        getUserById(id, (err, user) => {
+            
+            
+            expect(err).toBeUndefined();
+            expect(user?.id).toBe(id);
+            
+            expect(user).toEqual({
+                id: 1,
+                name: 'John',
+                age: 25,
+                hobbies: ['reading', 'coding', 'hiking'],
+                isActive: true,
+            });
+
+            expect(user?.name).toBe('John');
+            
+            expect(user?.hobbies).toContain('reading');
+            
+            expect(user?.hobbies?.length).toBe(3);
+
+            done();
+        });
+
+    })
+
 })
