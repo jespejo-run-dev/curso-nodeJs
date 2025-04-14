3b74d45b1f3d5b55f3577956600038ce4af99633
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-04-14 10:54:27 -0400

    Arrage, Act, Assert

--- 02-bases/tests/app.test.ts
+++ 02-bases/tests/app.test.ts
@@ -1,7 +1,17 @@
-describe('Test', () => {
+describe('Test in the App File', () => {
 
-  test('should pass', () => {
-    expect(true).toBe(true);
+  test('should be 30', () => {
+
+    // Arrange
+    const num1 = 10;
+    const num2 = 20;
+
+    // Act
+    const result = num1 + num2;
+
+    // Assert
+    expect(result).toBe(30);
+    
   });
 
 });
